2331c5c84c1d31a1ec6ee3342240802ae04314cb
Gihwan Kim <ghkim3221@gmail.com>
2017-08-31 15:17:10 +0900

    Implement parsing command line arguments

--- rpi/startup.py
+++ rpi/startup.py
@@ -1,2 +1,23 @@
+import argparse
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='Auto Tracking CCTV RPi')
+
+    parser.add_argument('-i', '--ip', action='store',
+                        help='gateway\'s ip address')
+    parser.add_argument('-p', '--port', action='store', type=int,
+                        help='gateway\'s port')
+
+    parser.add_argument('--width', action='store', type=int,
+                        help='rpi camera width resolution')
+    parser.add_argument('--height', action='store', type=int,
+                        help='rpi camera height resolution')
+    parser.add_argument('--framerate', action='store', type=int,
+                        help='rpi camera framerate')
+
+    return parser.parse_args()
+
+
 def start_from_command_line():
     pass
