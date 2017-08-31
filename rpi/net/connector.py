75cbcb2ae2ff41da610bd591d19ff15067aca06f
Gihwan Kim <ghkim3221@gmail.com>
2017-08-31 16:32:19 +0900

    Try RPi application to connect with gateway until connected

--- rpi/net/connector.py
+++ rpi/net/connector.py
@@ -1,3 +1,18 @@
+import logging
+import socket
+
+logger = logging.getLogger(__name__)
+
+
 class GatewayConnector(object):
     def __init__(self, address):
         self.address = address
+        self.__socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.__socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, True)
+        self.__socket.setsockopt(socket.SOL_SOCKET, socket.TCP_NODELAY, True)
+
+    def try_connect(self):
+        logger.debug('Try to connect gateway at {}'.format(self.address))
+
+        self.__socket.connect(self.address)
+        self.address = self.__socket.getsockname()
