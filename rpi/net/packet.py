dc6e2d9ac08fd1b92add13e92f72de158375ac46
gihwan <0x1306e6d@gmail.com>
2017-09-01 11:16:44 +0900

    Handle packets from gateway

---
+++ rpi/net/packet.py
@@ -0,0 +1,33 @@
+import enum
+import struct
+
+
+class Opcode(enum.Enum):
+    SETUP = 0x01
+    RECORD = 0x10
+    PAUSE = 0x11
+    FRAME = 0x12
+    MOVE = 0x20
+    MOVED = 0x21
+
+
+def encode_packet(opcode, body):
+    opcode_size = struct.calcsize('!H')
+    opcode = struct.pack('!H', opcode.value)
+    body_size = len(body)
+
+    packet_size = opcode_size + body_size
+    packet_size = struct.pack('!L', packet_size)
+    packet = packet_size + opcode + body
+
+    return packet
+
+
+def decode_packet(packet):
+    opcode_size = struct.calcsize('!H')
+    opcode = packet[:opcode_size]
+    opcode = struct.unpack('!H', opcode)[0]
+    opcode = Opcode(opcode)
+    body = packet[opcode_size:]
+
+    return opcode, body
